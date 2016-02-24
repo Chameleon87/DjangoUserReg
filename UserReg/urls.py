4fa2d702d5b63163b91c44bfc91c12e63d55f88e
Chameleon87 <jessehodge1987@gmail.com>
2016-02-24 01:14:15 -0600

    Adding registration functionality

--- UserReg/urls.py
+++ UserReg/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include, patterns
 from django.contrib import admin
+from . import views 
 
-urlpatterns = [
-        url(r'^$/', index),
+urlpatterns = patterns('',
+        url(r'^$', views.index),
+        url(r'^blog', include(accounts.urls)),
     url(r'^admin/', admin.site.urls),
-]
+)
