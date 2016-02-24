2566d014356f1d63a3744f6a38f0d93c133d630d
Chameleon87 <jessehodge1987@gmail.com>
2016-02-24 03:23:57 -0600

    Adding login functionality

--- UserReg/views.py
+++ UserReg/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.contrib import auth
+from django.template.context_processors import csrf
 
 def index(request):
     return render_to_response('index.html', RequestContext(request))
+
+def login(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('accounts/login.html', c)
+
+def auth_view(request):
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None:
+        auth.login(request, user)
+        return HttpResponseRedirect('accounts/loggedin')
+    else:
+        return HttpResponseRedirect('accounts/invalid')
+
+def loggedin(request):
+    return render_to_response('accounts/loggedin.html',
+            {'full_name': request.user.username})
+
+def invalid_login(request):
+    return render_to_response('accounts/invalid_login.html')
+
+def logout(request):
+    auth.logout(request)
+    render_to_response('accounts/logout.html')
---
+++ accounts/forms.py
@@ -0,0 +1,4 @@
+from __future__ import unicode_literals
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
