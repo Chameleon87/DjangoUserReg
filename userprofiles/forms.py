8c8627033eecc80305b18f11a76b2ea8b540a763
Chameleon87 <jessehodge1987@gmail.com>
2016-02-27 17:44:07 -0600

    added userprofile functionality

--- userprofiles/forms.py
+++ userprofiles/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from userprofiles.models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
 
     class Meta:
         model = UserProfile
-        fields = ('user',)
+        fields = ('email', 'first_name', 'last_name',)
