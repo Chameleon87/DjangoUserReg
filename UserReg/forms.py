16745b7121d5a993293c5f23f5922059a5f0c3d7
chameleon87 <jessehodge1987@gmail.com>
2016-02-27 07:32:58 +0000

    getting rid of duplicate UserProfile models, views and forms

--- UserReg/forms.py
+++ UserReg/forms.py
@@ -1,9 +1,7 @@
 from __future__ import unicode_literals
 
-from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from UserReg.models import UserProfile
 
 class RegistrationForm(UserCreationForm):
 
@@ -19,31 +17,3 @@ class RegistrationForm(UserCreationForm):
                 user.save()
 
             return user
-
-class UserProfileForm(forms.ModelForm):
-    email = forms.EmailField(label=u'Email')
-    first_name = forms.CharField(label=u'First Name')
-    last_name = forms.CharField(label=u'Last Name')
-
-    def __init__(self, *args, **kw):
-        super(UserProfileForm, self).__init__(*args, **kw)
-        self.fields['email'].initial = self.instance.user.email
-        self.fields['first_name'].initial = self.instance.user.first_name
-        self.fields['last_name'].initial = self.instance.user.last_name
-
-        self.fields.keyOrder = [
-            'email',
-            'first_name',
-            'last_name',
-            ]
-
-    def save(self, *args, **kw):
-        super(UserProfileForm, self).save(*args, **kw)
-        User.email = self.cleaned_data.get('email')
-        User.first_name = self.cleaned_data.get('first_name')
-        User.last_name = self.cleaned_data.get('last_name')
-        User.save()
-
-    class Meta:
-        model = UserProfile
-        fields = ('email', 'first_name', 'last_name')
