5401f65c30c179f5dc8734187965df6d01c5d744
Chameleon87 <jessehodge1987@gmail.com>
2016-02-27 22:55:54 -0600

    fixed the RegistrationForm to initiate more fields

--- UserReg/forms.py
+++ UserReg/forms.py
@@ -1,17 +1,19 @@
 from __future__ import unicode_literals
 
-from django.contrib.auth.models import User
+from userprofiles.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
 
         def save(self, commit=True):
             user = super(RegistrationForm, self).save(commit=False)
             user.email = self.cleaned_data['email']
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
 
             if commit:
                 user.save()
--- userprofiles/models.py
+++ userprofiles/models.py
@@ -4,13 +4,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-    def __unicode__(self):
-        return str(self)
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="User_Profile")
     email = models.EmailField()
