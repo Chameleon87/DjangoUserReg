d2b8e7a067b2b54400457dd37a8b736812ff6589
Chameleon87 <jessehodge1987@gmail.com>
2016-02-28 03:33:01 -0600

    Fixing stuff... the edit_profile function isn't working correctly

--- userprofiles/forms.py
+++ userprofiles/forms.py
@@ -2,15 +2,16 @@ from django import forms
 from userprofiles.models import UserProfile
 
 class UserProfileForm(forms.ModelForm):
+    email = forms.EmailField()
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
 
     class Meta:
         model = UserProfile
-        fields = ('first_name', 'last_name')
+        fields = ('user' ,'first_name', 'last_name')
 
         def save(self, commit=True):
-            userprofile = super(UserProfile, self).save(commit=False)
+            userprofile = super(UserProfile, self).save(commit=True)
             user.email = self.cleaned_data['email']
             user.first_name = self.cleaned_data['first_name']
             user.last_name = self.cleaned_data['last_name']
