8bd2193d2f11d5185adee4f43a651f47c403dd42
chameleon87 <jessehodge1987@gmail.com>
2016-02-29 17:27:25 +0200

    Rid of unnecessary code

--- UserReg/forms.py
+++ UserReg/forms.py
@@ -1,21 +1,10 @@
 from __future__ import unicode_literals
-
-from userprofiles.models import User
 from django.contrib.auth.forms import UserCreationForm
+from userprofiles.models import User
+
 
 class RegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-
-        def save(self, commit=True):
-            user = super(RegistrationForm, self).save(commit=False)
-            user.email = self.cleaned_data['email']
-            user.first_name = self.cleaned_data['first_name']
-            user.last_name = self.cleaned_data['last_name']
-
-            if commit:
-                user.save()
-
-            return user
