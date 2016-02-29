9f6cdd32b76e7e4e2b82bf3123cfb7b94da6a33d
chameleon87 <jessehodge1987@gmail.com>
2016-02-29 23:06:03 +0200

    Edit_article change

--- article/urls.py
+++ article/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url, include
-from article.views import articles, article, create_article
+from article.views import articles, article, edit_article
 
 urlpatterns = [
     url(r'^$', articles, name='all'),
     url(r'^all/$', articles, name='all'),
-    url(r'^get/(?P<article_id>\d+)/$', article, name='get'),
-    url(r'^create/$', create_article, name='create'),
+    url(r'^(?P<article_id>\d+)/$', article, name='get'),
+    url(r'^create/$', edit_article, name='create'),
     #App urls
     url(r'^accounts/', include('userprofiles.urls')),
 ]
