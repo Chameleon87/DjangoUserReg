0f40a9c1fabe5e7cd012d5dc6f221bd4b50647f5
Chameleon87 <jessehodge1987@gmail.com>
2016-03-01 19:41:49 -0600

    most recent changes

--- article/models.py
+++ article/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     body = models.TextField()
 
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return "/articles/get/%i" % self.pk
--- article/urls.py
+++ article/urls.py
@@ -3,7 +3,7 @@ from article.views import articles, article, edit_article
 
 urlpatterns = [
     url(r'^all/$', articles, name='all'),
-    url(r'^get/(?P<pk>\d+)/$', article, name='get'),
+    url(r'^get/(?P<article_id>\d+)/$', article, name='get'),
     url(r'^create/$', edit_article, name='create'),
     #App urls
     url(r'^accounts/', include('userprofiles.urls')),
