6e0909741deb00f9b1e053f19a1e2f9afb1b1d5a
Chameleon87 <jessehodge1987@gmail.com>
2016-03-01 20:20:36 -0600

    Done testing userprofiles views

--- userprofiles/views.py
+++ userprofiles/views.py
@@ -11,21 +11,16 @@ def user_profile(request):
 @login_required
 def edit_user_profile(request):
     if request.method == "POST":
-        print "a"
         form = UserProfileForm(data=request.POST, instance=request.user.profile)
         if form.is_valid():
-            print "b"
             form.save()
             return render(request, 'accounts/user_profile.html')
         else:
-            print "c"
             return render(request, 'accounts/edit_profile.html', {"form" : form })
     else:
-        print "e"
         form = UserProfileForm()
         if form.is_valid():
             form.save()
             return render(request, 'accounts/user_profile.html')
         else:
-            print "d"
             return render(request, 'accounts/edit_profile.html', {"form" : form })
