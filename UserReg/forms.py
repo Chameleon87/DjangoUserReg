47965a33d769df508a2422e502595e697c4f1414
Chameleon87 <jessehodge1987@gmail.com>
2016-03-01 20:50:54 -0600

    Final commit until next addition to the project

--- UserReg/forms.py
+++ UserReg/forms.py
@@ -1,10 +1,9 @@
 from __future__ import unicode_literals
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
 
 class RegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        fields = ('__all__')
