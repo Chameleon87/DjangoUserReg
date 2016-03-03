c35482ece93cd2a17d94b9db3579f44702ce55d6
Chameleon87 <jessehodge1987@gmail.com>
2016-03-03 03:14:28 -0600

    more heroku deployment functionality

--- settings.py
+++ settings.py
@@ -21,9 +21,9 @@ SECRET_KEY = '7pkxdk20+pknopt%%kzqesptnq*u@*@8&!((f)gk#yd6t44kg1'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 AUTH_PROFILE_MODULE = 'userprofile.UserProfile'
