68beb4d0de8ac674c18154536e30b7cf86982291
chameleon87 <jessehodge1987@gmail.com>
2016-03-03 21:00:01 +0000

    Fixed UserProfile

--- UserReg/forms.py
+++ UserReg/forms.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
