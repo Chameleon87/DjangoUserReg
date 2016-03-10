5218331da0139ddd7ec3625468a4beef35adc647
Chameleon87 <jessehodge1987@gmail.com>
2016-03-09 19:45:15 -0600

    Awesome settings

---
+++ local_settings.py
@@ -0,0 +1,4 @@
+from settings import *
+
+SECRET_KEY = 'hft^xe(+(&v1ypkrmryxk-%c*3o$^6=ed^3jsnyn@qwkl2g68-'
+ALLOWED_HOSTS = []
--- settings.py
+++ settings.py
@@ -18,15 +18,8 @@ BASE_DIR = os.path.dirname(__file__)
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
-CSRF_COOKIE_SECURE = True
-CSRF_COOKIE_HTTPONLY = True
-SESSION_COOKIE_SECURE = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_SSL_REDIRECT = True
-X_FRAME_OPTIONS = 'DENY'
 # Application definition
 
 INSTALLED_APPS = [
