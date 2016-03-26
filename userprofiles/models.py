f0eded36ea547b119995a0918650afc05113c501
Chameleon87 <jessehodge1987@gmail.com>
2016-03-26 12:33:17 -0500

    Adding Address to the userprofile Model

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -10,6 +10,13 @@ class UserProfile(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
+    #Address
+    address1 = models.CharField(max_length=100)
+    address2 = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=20)
+
     def __unicode__(self):
         return str(self)
 
