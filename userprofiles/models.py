c13372ef217958613acdc55ae00887226f436059
Chameleon87 <jessehodge1987@gmail.com>
2016-03-26 12:43:04 -0500

    Adding more on userprofile

--- userprofiles/models.py
+++ userprofiles/models.py
@@ -9,6 +9,7 @@ class UserProfile(models.Model):
     email = models.EmailField()
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=15)
 
     #Address
     address1 = models.CharField(max_length=100)
@@ -16,6 +17,9 @@ class UserProfile(models.Model):
     city = models.CharField(max_length=50)
     state = models.CharField(max_length=50)
     zip_code = models.CharField(max_length=20)
+
+    #Confidential
+    ssn = models.CharField(max_length=12)
 
     def __unicode__(self):
         return str(self)
